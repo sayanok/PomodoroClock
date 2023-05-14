9dc1d8bed907fde64baf4317e6b1d306914f6cdb
sayanok <rng.jhn.osca@gmail.com>
2023-05-14 13:25:34 +0900

    add カウントダウン機能

--- pomodoro-clock/src/App.tsx
+++ pomodoro-clock/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [timer, setTimer] = useState(3);
+  const [session, setSession] = useState<"作業セッション" | "休憩セッション">("作業セッション");
+  // const [timeoutId, setTimeoutId] = useState<NodeJS.Timer>();
+  let timeoutId = useRef<NodeJS.Timer>();
+
+  useEffect(() => {
+    if (timer === 0) {
+      clearInterval(timeoutId.current);
+    }
+  }, [timer]);
+
+  function onClickHandler() {
+    // timerStart();
+    countdown();
+  }
+
+  // function timerStart() {
+  //   setTimeout(() => {
+  //     changeTimer();
+  //   }, timer);
+  // }
+
+  function countdown() {
+    timeoutId.current = setInterval(() => {
+      setTimer((timer) => timer - 1);
+    }, 1000);
+  }
+
+  function changeTimer() {
+    console.log("changeTimer");
+    // セッションを変更する
+    // タイマーを切り替える
+  }
+
+  function pause() {
+    // タイマーを一時停止する
+  }
+
+  function reset() {
+    setSession("作業セッション");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>タイマー：{timer}</p>
+      <p>現在のセッション:{session}</p>
+      <button onClick={() => onClickHandler()}>開始｜停止</button>
+      <button onClick={() => reset()}>リセット</button>
+    </>
   );
-}
+};
 
 export default App;
